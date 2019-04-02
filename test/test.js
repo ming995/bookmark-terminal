b9ab54438b3bf70275f988fe4e001c4e41ee8846
ming995 <hymghost@gmail.com>
2019-04-02 17:40:36 +0800

    [chore] set babel and test

---
+++ test/test.js
@@ -0,0 +1,11 @@
+/**
+ *  测试代码
+ */
+
+import argv from "yargs";
+
+if (argv.ships > 3 && argv.distance < 53.5) {
+    console.log('Plunder more riffiwobbles!')
+} else {
+    console.log('Retreat from the xupptumblers!')
+}
